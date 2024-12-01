c5daeec11e860779f91fc5bab866b12b7543b15d
Yoonha Kim <40644178+Ghostlun@users.noreply.github.com>
2024-12-01 18:46:29 -0500

    update

--- APIService/app.py
+++ APIService/app.py
@@ -90,5 +90,5 @@ def predict_diagnosis():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=False)
